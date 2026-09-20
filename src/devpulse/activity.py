4c2695b39e71ebac8de2a4aff921ec787d6b513b
pstrings <masuya444@gmail.com>
2026-09-21 01:08:41 +0530

    activity: created a temporary dictionary session

--- src/devpulse/activity.py
+++ src/devpulse/activity.py
@@ -2,11 +2,11 @@ import time
 
 import pywinctl as pwc
 
-from devpulse.decorators import start_times_tamp
+from devpulse.decorators import start_time_stamp
 
 
-@start_times_tamp
-def get_active_window_title():
+@start_time_stamp
+def get_active_window():
     active_window = pwc.getActiveWindow()
     if active_window:
         return active_window
@@ -14,27 +14,44 @@ def get_active_window_title():
         return None
 
 
-previous_window = None
+previous_title = None
+previous_handle = None
+previous_app_name = None
+previous_pid = None
 previous_start_time = None
+session = []
 
-while True:
-    current_window, observed_at = get_active_window_title()
+try:
+    while True:
+        current_window, observed_at = get_active_window()
+        current_title = current_window.title if current_window else None
+        current_handle = current_window.getHandle() if current_window else None
+        current_pid = current_window.getPID() if current_window else None
+        current_app_name = current_window.getAppName() if current_window else None
 
-    if current_window and current_window != previous_window:
-        print(
-            f"Focused window title changed: {current_window.title} at {observed_at}")
-        if previous_window and previous_start_time:
-            print(f"""
-            Session ended
-            Application: {previous_window.getAppName()}
-            Window: {previous_window.title}
-            Window Handle: {previous_window.getHandle()}
-            PID: {previous_window.getPID()}
-            Start time: {previous_start_time}
-            End time: {observed_at}
-            Duration: {observed_at - previous_start_time}
-            """)
-        previous_window = current_window
-        previous_start_time = observed_at
+        if current_title and (current_handle != previous_handle or current_title != previous_title):
+            if previous_title and previous_start_time:
+                session.append({
+                    "application": previous_app_name,
+                    "pid": previous_pid,
+                    "window_handle": previous_handle,
+                    "window_title": previous_title,
+                    "start_time": previous_start_time,
+                    "end_time": observed_at,
+                    "duration": observed_at - previous_start_time
+                })
 
-    time.sleep(0.25)
+            print(
+                f"Focused window title changed: {current_window.title} at {observed_at}")
+
+            previous_title = current_title
+            previous_handle = current_handle
+            previous_pid = current_pid
+            previous_app_name = current_app_name
+            previous_start_time = observed_at
+
+        time.sleep(0.25)
+except KeyboardInterrupt:
+    print("Exiting...")
+finally:
+    print(session)
--- src/devpulse/decorators.py
+++ src/devpulse/decorators.py
@@ -2,10 +2,10 @@ import datetime
 from functools import wraps
 
 
-def start_times_tamp(function):
+def start_time_stamp(function):
     @wraps(function)
     def wrapper(*args, **kwargs):
         observed_at = datetime.datetime.now(datetime.UTC)
         result = function(*args, **kwargs)
-        return (result, observed_at)
+        return result, observed_at
     return wrapper
